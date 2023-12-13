2f2ead2819cf71bbf2ff10e4df015ad37fc03e95
itzikbt <94228116+itzikbt@users.noreply.github.com>
2023-12-13 20:27:11 +0200

    Good Luck!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import static TicTacToe_package.TicTacToe.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-','+','-','+','-'},{' ', '|', ' ', '|', ' '},
+                {'-','+','-','+','-'},{' ', '|', ' ', '|', ' '}};
+
+        while(true) {
+            printGameBoard(gameBoard);
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter your placement (1-9):"); // 1-9 are the positions on the board from left to right
+            int playerPos = scan.nextInt();
+            while(playersPositions.contains(playerPos) || cpuPositions.contains(playerPos) || playerPos > 9 || playerPos < 1 ){
+                System.out.println("Position not valid! Enter a correct position");
+                playerPos = scan.nextInt();
+            }
+            playersPositions.add(playerPos);
+            placePiece(gameBoard, playerPos, "player");
+
+            String gamePos = checkWinner(); //check if the player won or it's a cat
+
+            if (gamePos.equals("")) {
+                Random rand = new Random();
+                int cpuPos = rand.nextInt(9) + 1;
+                while (playersPositions.contains(cpuPos) || cpuPositions.contains(cpuPos))
+                    cpuPos = rand.nextInt(9) + 1;
+                cpuPositions.add(cpuPos);
+
+                placePiece(gameBoard, cpuPos, "cpu");
+            }
+
+            gamePos = checkWinner(); // check if the cpu won
+
+            if(!gamePos.equals("")) { // check winner not returning "" unless the game finished
+                printGameBoard(gameBoard);
+                System.out.printf("%s\n", gamePos);
+                System.out.println("Would you like to play another round? Answer with y/n");
+                String answer = scan.next();
+                while (!answer.equals("y") && !answer.equals("n")){
+                    System.out.println("Answer not valid! Enter a correct answer");
+                    answer = scan.next();
+                }
+                if (answer.equals("y")){
+                    restartGame(gameBoard);
+                }else {
+                    System.out.println("GoodBye :)");
+                    break;
+                }
+            }
+
+            System.out.println();
+        }
+
     }
 }
--- src/TicTacToe_package/TicTacToe.java
+++ src/TicTacToe_package/TicTacToe.java
@@ -1,58 +1,10 @@
+package TicTacToe_package;
+
 import java.util.*;
 public class TicTacToe {
-    public static void main(String[] args) {
-        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
-                {'-','+','-','+','-'},{' ', '|', ' ', '|', ' '},
-                {'-','+','-','+','-'},{' ', '|', ' ', '|', ' '}};
 
-        while(true) {
-            printGameBoard(gameBoard);
-            Scanner scan = new Scanner(System.in);
-
-            System.out.println("Enter your placement (1-9):"); // 1-9 are the positions on the board from left to right
-            int playerPos = scan.nextInt();
-            while(playersPositions.contains(playerPos) || cpuPositions.contains(playerPos) || playerPos > 9 || playerPos < 1 ){
-                System.out.println("Position not valid! Enter a correct position");
-                playerPos = scan.nextInt();
-            }
-            playersPositions.add(playerPos);
-            placePiece(gameBoard, playerPos, "player");
-
-            String gamePos = checkWinner(); //check if the player won or it's a cat
-
-            if (gamePos.equals("")) {
-                Random rand = new Random();
-                int cpuPos = rand.nextInt(9) + 1;
-                while (playersPositions.contains(cpuPos) || cpuPositions.contains(cpuPos))
-                    cpuPos = rand.nextInt(9) + 1;
-                cpuPositions.add(cpuPos);
-
-                placePiece(gameBoard, cpuPos, "cpu");
-            }
-
-            gamePos = checkWinner(); // check if the cpu won
-
-            if(!gamePos.equals("")) { // check winner not returning "" unless the game finished
-                printGameBoard(gameBoard);
-                System.out.printf("%s\n", gamePos);
-                System.out.println("Would you like to play another round? Answer with y/n");
-                String answer = scan.next();
-                while (!answer.equals("y") && !answer.equals("n")){
-                    System.out.println("Answer not valid! Enter a correct answer");
-                    answer = scan.next();
-                }
-                if (answer.equals("y")){
-                    restartGame(gameBoard);
-                }else {
-                    System.out.println("GoodBye :)");
-                    break;
-                }
-            }
-
-            System.out.println();
-        }
-
-    }
+    public static ArrayList<Integer> playersPositions = new ArrayList<>(); // the positions the player chose
+    public static ArrayList<Integer> cpuPositions = new ArrayList<>(); // the positions the cpu chose
 
 
     public static void restartGame(char[][] gameBoard){
@@ -69,10 +21,6 @@ public class TicTacToe {
         playersPositions.clear();
         cpuPositions.clear();
     }
-
-
-    static ArrayList<Integer> playersPositions = new ArrayList<>(); // the positions the player chose
-    static ArrayList<Integer> cpuPositions = new ArrayList<>(); // the positions the cpu chose
 
 
     public static String checkWinner(){
